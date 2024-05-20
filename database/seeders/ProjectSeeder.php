869b271d63a7d98f93a4a4b1df1fba530659688e
SimoneCerri <simone.cerri1994@gmail.com>
2024-05-20 15:42:36 +0200

    add faker and str in ProjectSeeder.php, fill with a for loop for 10 new projects, add method call() in DatabaseSeeder aswell

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -4,14 +4,23 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
 class ProjectSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        //
+        for ($i=0; $i < 10 ; $i++) { 
+            $project = new Project();
+            $project->title = $faker->words(3,true);
+            $project->slug = Str::of($project->title)->slug('-');
+            $project->content = $faker->text(100);
+            $project->img = $faker->imageUrl(240,150,'Projects',true,$project->title,true,'png');
+            $project->save();
+        }
     }
 }
