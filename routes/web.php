c476b5fe8bf3ce56953f482f41acd1508f872cef
SimoneCerri <simone.cerri1994@gmail.com>
2024-05-20 15:56:57 +0200

    add resources route for projects in web.php and the return of the view(index) in ProjectController.php

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\ProjectController;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,6 +27,8 @@ Route::middleware(['auth','verified'])
     {
         //all route here that needs to be protected by our auth system
         Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+
+        Route::resource('projects',ProjectController::class);
     });
 
 Route::middleware('auth')->group(function () {
