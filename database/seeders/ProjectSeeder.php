078da546698757027cfea088a4c6bb91c494066b
SimoneCerri <simone.cerri1994@gmail.com>
2024-05-21 11:47:03 +0200

    add 2 more columns in projects table (db), migrate and seed with some Faker

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -15,12 +15,14 @@ class ProjectSeeder extends Seeder
      */
     public function run(Faker $faker): void
     {
-        for ($i=0; $i < 10 ; $i++) { 
+        for ($i = 0; $i < 10; $i++) {
             $project = new Project();
-            $project->title = $faker->words(3,true);
+            $project->title = $faker->words(3, true);
             $project->slug = Str::of($project->title)->slug('-');
             $project->content = $faker->text(100);
-            $project->img = $faker->imageUrl(240,150,'Projects',true,$project->title,true,'png');
+            $project->img = $faker->imageUrl(240, 150, 'Projects', true, $project->title, true, 'png');
+            $project->url1 = $faker->url();
+            $project->url2 = $faker->url();
             $project->save();
         }
     }
